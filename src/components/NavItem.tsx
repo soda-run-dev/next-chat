dc82be8a952f2d7e311141a4830fa38ec528e34c
soda <soda.run.dev@gmail.com>
2024-02-04 18:22:24 +0900

    Next Auth 설정
    - SessionProvider 설정
    - useSession 정상 동작 확인

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,7 +1,11 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavItem = ({ mobile }: { mobile?: boolean }) => {
+  const { data: session, status } = useSession();
+  console.log("session", session);
+
   return (
     <ul
       className={`text-md justify-center flex w-full gap-4 ${
@@ -14,12 +18,15 @@ const NavItem = ({ mobile }: { mobile?: boolean }) => {
       <li className={`py-2 text-center border-b-4 cursor-pointer`}>
         <Link href={"/user"}>User</Link>
       </li>
-      <li className={`py-2 text-center border-b-4 cursor-pointer`}>
-        <button>Signout</button>
-      </li>
-      <li className={`py-2 text-center border-b-4 cursor-pointer`}>
-        <button>Signin</button>
-      </li>
+      {session?.user ? (
+        <li className={`py-2 text-center border-b-4 cursor-pointer`}>
+          <button onClick={() => signOut()}>Signout</button>
+        </li>
+      ) : (
+        <li className={`py-2 text-center border-b-4 cursor-pointer`}>
+          <button onClick={() => signIn()}>Signin</button>
+        </li>
+      )}
     </ul>
   );
 };
