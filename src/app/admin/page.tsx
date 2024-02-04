342a6f7e57ac7b5a486c6da5dc9b0600a4d62bbc
soda <soda.run.dev@gmail.com>
2024-02-04 16:15:30 +0900

    관리자, 유저 페이지 생성

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const admin = () => {
+  return <div>관리자 페이지</div>;
+};
+
+export default admin;
---
+++ src/app/user/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const user = () => {
+  return <div>로그인 유저만 볼 수 있음</div>;
+};
+
+export default user;
